33d2d456bf92a9597a98e4f1fdcb4167dc0ef4c5
Pacheco <ricardomartins@Ricardos-MacBook-Pro.local>
2017-02-28 12:03:48 +0000

    First Commit

---
+++ Chess/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Chess
+//
+//  Created by Ricardo Martins on 28/02/2017.
+//  Copyright © 2017 Ricardo Martins. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Chess\n";
+    
+    
+    
+    
+    
+    return 0;
+}
